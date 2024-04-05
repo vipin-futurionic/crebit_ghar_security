6a4db19e5c744963f51269b089165ef76674a5b4
vipin-futurionic <vipin.futurionic.com>
2024-04-05 18:04:22 +0530

    ssl certificate add  but not showing not vaild

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const nocache = require("nocache");
+const helmet = require("helmet");
 
 const securityMiddleware = require("./middleware/securityMiddleware");
 const mainController = require("./controllers/mainController");
@@ -13,6 +14,19 @@ app.use(securityMiddleware.useHelmet());
 // Enable HTTP Strict Transport Security (HSTS) to force the use of HTTPS
 app.use(securityMiddleware.enableHSTS());
 
+// Prevents browsers from trying to guess (incorrectly) the MIME type
+// of a response, based on the content itself. By setting the X-Content-Type-Options
+// header to nosniff, the browser will not guess the type and will instead
+// follow the type specified in the Content-Type header.
+app.use(helmet.noSniff());
+
+// Prevents Internet Explorer from executing downloads in the context of a
+// protected mode process. By setting the X-Download-Options header to noopen,
+// the browser will instead open downloaded files in a new process.
+// This helps prevent exploits that may affect the integrity of the current
+// process, for example, a malicious PDF opened by the browser.
+app.use(helmet.ieNoOpen());
+
 // Middleware to add security headers
 app.use(nocache());
 
@@ -22,7 +36,7 @@ app.use(nocache());
 // Define routes
 app.get("/", mainController.index);
 
-const port = 2000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
